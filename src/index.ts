9321a3745d527e5bfccb72745a39850b3f827778
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-10-04 10:22:51 -0300

    chore: config eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const add = (a: number, b: number) => {
-    return a + b;
+  return a + b
 }
 
 const number1: number = 5
 const number2: number = 10
 const result: number = add(number1, number2)
 
-console.log('The result is %d', result)
+console.log("The result is: %d", result)
