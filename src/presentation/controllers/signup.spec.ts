9ac5b7af59b075fb27c44dea94a0af81569f3a30
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-10-10 17:33:33 -0300

    refactor(signup): ensure signup controller implements correct interface

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from "./signUp"
+import { SignUpController } from "./signup"
 
 describe("SignUp Controller", () => {
   test("should return 400 if no name is provided", () => {
